a7547736aa50c664885a18241bc7f2cea79b52df
alihkz94 <94843557+alihkz94@users.noreply.github.com>
2023-08-31 10:21:30 +0300

    Create UCHIME.py
    How to create UCHIME database format from an Excel file

---
+++ formats/UCHIME.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+def excel_to_fasta_specified_header(excel_file_path, fasta_file_path):
+    # Step 1: Read the Excel file
+    df = pd.read_excel(excel_file_path)
+    
+    # Step 2: Create specific FASTA headers
+    fasta_content = ""
+    for index, row in df.iterrows():
+        header = f"{row['acc_new']}|k__{row['kgd']};p__{row['ph']};c__{row['c']};o__{row['o']};f__{row['f']};g__{row['g']};s__{row['s']}"
+        sequence = row['seq']
+        
+        # Step 3: Append to FASTA content
+        fasta_content += f">{header}\n{sequence}\n"
+        
+    # Step 4: Write to FASTA file
+    with open(fasta_file_path, 'w') as f:
+        f.write(fasta_content)
+
+# Usage
+excel_file_path = 'path/to/your/excel/file.xlsx'
+fasta_file_path = 'path/to/your/output/fasta/file.fasta'
+excel_to_fasta_specified_header(excel_file_path, fasta_file_path)
