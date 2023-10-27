8fd670436c42724939fd6ce9983f0d82e0667691
Ali Hakimzadeh <94843557+alihkz94@users.noreply.github.com>
2023-10-27 16:24:59 +0300

    Update QIIME2.py

--- formats/QIIME2.py
+++ formats/QIIME2.py
@@ -21,3 +21,35 @@ def excel_to_new_fasta_format(excel_file_path, fasta_file_path):
 excel_file_path = 'path/to/your/excel/file.xlsx'
 fasta_file_path = 'path/to/your/new/output/fasta/file.fasta'
 excel_to_new_fasta_format(excel_file_path, fasta_file_path)
+
+
+def excel_to_qiime2_tsv(excel_file_path, tsv_file_path):
+    """
+    Convert an Excel file to a TSV file compatible with QIIME2's FeatureData[Taxonomy] format.
+    
+    Parameters:
+    excel_file_path (str): Path to the input Excel file containing taxonomy and sequence information.
+    tsv_file_path (str): Path to the output TSV file compatible with QIIME2.
+    
+    Returns:
+    None
+    """
+    # Step 1: Read the Excel file
+    df = pd.read_excel(excel_file_path)
+    
+    # Step 2: Create DataFrame for TSV format
+    feature_ids = df['acc_new']
+    taxons = df.apply(lambda row: f"k__{row['kgd']};p__{row['ph']};c__{row['c']};o__{row['o']};f__{row['f']};g__{row['g']};s__{row['s']}", axis=1)
+    
+    tsv_df = pd.DataFrame({
+        'Feature ID': feature_ids,
+        'Taxon': taxons
+    })
+    
+    # Step 3: Write to TSV file
+    tsv_df.to_csv(tsv_file_path, sep='\t', index=False)
+
+# Usage example (Note: The actual file paths should be provided)
+excel_file_path = 'path/to/your/excel/file.xlsx'
+tsv_file_path = 'path/to/your/new/output/tsv/file.tsv'
+excel_to_qiime2_tsv(excel_file_path, tsv_file_path)
