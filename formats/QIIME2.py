0e17f9f3774fad4f3b608c55f77c0a57682de763
Ali Hakimzadeh <94843557+alihkz94@users.noreply.github.com>
2023-10-28 21:43:07 +0300

    Update QIIME2.py

--- formats/QIIME2.py
+++ formats/QIIME2.py
@@ -1,55 +1,45 @@
 import pandas as pd
 
-def excel_to_new_fasta_format(excel_file_path, fasta_file_path):
-    # Step 1: Read the Excel file
+def clean_sequence(sequence):
+    """
+    Remove non-standard characters from a DNA sequence and convert to uppercase.
+    """
+    allowed_chars = set('ACTG')
+    return ''.join(c.upper() for c in sequence if c.upper() in allowed_chars)
+
+def format_sequence(seq, line_length=80):
+    """
+    Break down long sequence lines into shorter segments.
+    """
+    return '\n'.join(seq[i:i+line_length] for i in range(0, len(seq), line_length))
+
+def generate_files(excel_file_path, fasta_file_path, tsv_file_path):
+    """
+    Generate FASTA and TSV files from an Excel file.
+    """
+    # Read Excel file
     df = pd.read_excel(excel_file_path)
     
-    # Step 2: Create FASTA headers in the new format
-    fasta_content = ""
-    for index, row in df.iterrows():
-        header = f"{row['acc_new']}    k__{row['kgd']};p__{row['ph']};c__{row['c']};o__{row['o']};f__{row['f']};g__{row['g']};s__{row['s']}"
-        sequence = row['seq']
-        
-        # Step 3: Append to FASTA content
-        fasta_content += f">{header}\n{sequence}\n"
-        
-    # Step 4: Write to new FASTA file
-    with open(fasta_file_path, 'w') as f:
-        f.write(fasta_content)
+    # Clean sequences
+    df['sequence'] = df['sequence'].apply(clean_sequence)
+    
+    # Generate FASTA file
+    with open(fasta_file_path, 'w') as f_fasta:
+        for idx, row in df.iterrows():
+            formatted_seq = format_sequence(row['sequence'])
+            f_fasta.write(f">{row['acc_new']}\n{formatted_seq}\n")
+    
+    # Generate TSV file
+    with open(tsv_file_path, 'w') as f_tsv:
+        f_tsv.write("Feature ID\tTaxon\n")
+        for idx, row in df.iterrows():
+            taxon = ';'.join([f"{k}__{('unclassified' if v == '.' else v)}" for k, v in row[['k', 'p', 'c', 'o', 'f', 'g', 's']].items()])
+            f_tsv.write(f"{row['acc_new']}\t{taxon}\n")
 
-# Usage
+# File paths
 excel_file_path = 'path/to/your/excel/file.xlsx'
-fasta_file_path = 'path/to/your/new/output/fasta/file.fasta'
-excel_to_new_fasta_format(excel_file_path, fasta_file_path)
+fasta_file_path = 'path/to/save/fasta/file.fasta'
+tsv_file_path = 'path/to/save/tsv/file.tsv'
 
-###Generating the TSV file
-def excel_to_qiime2_tsv(excel_file_path, tsv_file_path):
-    """
-    Convert an Excel file to a TSV file compatible with QIIME2's FeatureData[Taxonomy] format.
-    
-    Parameters:
-    excel_file_path (str): Path to the input Excel file containing taxonomy and sequence information.
-    tsv_file_path (str): Path to the output TSV file compatible with QIIME2.
-    
-    Returns:
-    None
-    """
-    # Step 1: Read the Excel file
-    df = pd.read_excel(excel_file_path)
-    
-    # Step 2: Create DataFrame for TSV format
-    feature_ids = df['acc_new']
-    taxons = df.apply(lambda row: f"k__{row['kgd']};p__{row['ph']};c__{row['c']};o__{row['o']};f__{row['f']};g__{row['g']};s__{row['s']}", axis=1)
-    
-    tsv_df = pd.DataFrame({
-        'Feature ID': feature_ids,
-        'Taxon': taxons
-    })
-    
-    # Step 3: Write to TSV file
-    tsv_df.to_csv(tsv_file_path, sep='\t', index=False)
-
-# Usage example (Note: The actual file paths should be provided)
-excel_file_path = 'path/to/your/excel/file.xlsx'
-tsv_file_path = 'path/to/your/new/output/tsv/file.tsv'
-excel_to_qiime2_tsv(excel_file_path, tsv_file_path)
+# Generate files
+generate_files(excel_file_path, fasta_file_path, tsv_file_path)
