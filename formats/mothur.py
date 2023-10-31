5557c7fd009b68d56cd37d4fbdb5c7df88c71b2f
Ali Hakimzadeh <94843557+alihkz94@users.noreply.github.com>
2023-10-31 20:54:41 +0200

    IUPAC characters updated and minor changes  added

--- formats/mothur.py
+++ formats/mothur.py
@@ -1,10 +1,12 @@
 import pandas as pd
 
 def clean_sequence(sequence):
-    allowed_chars = set('ACTG')
-    return ''.join(c.upper() for c in sequence if c.upper() in allowed_chars)
+    """Retain only IUPAC standard characters for DNA sequences and convert to uppercase."""
+    iupac_chars = set('ACGTRYKMSWBDHVN')
+    return ''.join(c.upper() if c.upper() in iupac_chars else c for c in sequence)
 
 def format_sequence(seq, line_length=80):
+    """Break down long sequence lines into shorter segments."""
     return '\n'.join(seq[i:i+line_length] for i in range(0, len(seq), line_length))
 
 def generate_mothur_files(excel_file_path, fasta_file_path, tax_file_path, chunk_size=5000):
@@ -26,10 +28,15 @@ def generate_mothur_files(excel_file_path, fasta_file_path, tax_file_path, chunk
         tax_data = []
         for idx, row in chunk.iterrows():
             formatted_seq = format_sequence(row['sequence'])
-            fasta_data.append(f">{row['acc_new']}\n{formatted_seq}")
-            taxon = '\t'.join([f"{row['acc_new']}", ';'.join([f"{k}__{('unclassified' if v == '.' else v)}" 
+            fasta_data.append(f">{row['acc_new']}_{idx}\n{formatted_seq}")
+            taxon = '\t'.join([f"{row['acc_new']}_{idx}", ';'.join([f"{k}__{('unclassified' if v == '.' else v)}" 
                                                                for k, v in row[['k', 'p', 'c', 'o', 'f', 'g', 's']].items()]) + ';'])
             tax_data.append(taxon)
+        
+        # Write this chunk's data to FASTA and tax files
+        with open(fasta_file_path, 'a') as f_fasta, open(tax_file_path, 'a') as f_tax:
+            f_fasta.write('\n'.join(fasta_data) + '\n')
+            f_tax.write('\n'.join(tax_data) + '\n')
         
         # Write this chunk's data to FASTA and tax files
         with open(fasta_file_path, 'a') as f_fasta, open(tax_file_path, 'a') as f_tax:
